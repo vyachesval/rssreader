a1bce81a9ba87baa2ee7b1b9d06ed2d8a782bdf0
Gavrilov V <viacheslav.gavrilov@domru.ru>
2020-11-04 20:09:37 +0300

    added opening a rss channel news urls in the browser

--- app/src/main/java/dev/rssreader/presentation/screen/list/news/RssChannelNewsListFragment.kt
+++ app/src/main/java/dev/rssreader/presentation/screen/list/news/RssChannelNewsListFragment.kt
@@ -1,5 +1,7 @@
 package dev.rssreader.presentation.screen.list.news
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,26 +14,29 @@ import dev.rssreader.R
 import dev.rssreader.domain.entity.RssChannelNewsData
 import dev.rssreader.presentation.screen.list.ItemClickListener
 import dev.rssreader.presentation.screen.list.ListFragment
+import dev.rssreader.presentation.screen.webview.WebViewActivity
 import kotlinx.android.synthetic.main.add_channel_button.view.*
+
 
 class RssChannelNewsListFragment : ListFragment() {
 
     private lateinit var viewModel: RssChannelNewsListViewModel
 
     private val adapter = RssChannelNewsListAdapter(
-        object: ItemClickListener<RssChannelNewsData> {
+        object : ItemClickListener<RssChannelNewsData> {
             override fun onClick(position: Int, item: RssChannelNewsData) {
                 Log.d("iszx", "RssChannelNewsListFragment opening news link" + item.link)
-                viewModel.getRssChannelNews(item.link)
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(item.link)))
             }
+
             override fun onLongClick(position: Int, item: RssChannelNewsData): Boolean {
                 return false
             }
         })
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         viewModel = provideViewModel(RssChannelNewsListViewModel::class.java)
         return inflater.inflate(R.layout.fragment_list, container, false)
